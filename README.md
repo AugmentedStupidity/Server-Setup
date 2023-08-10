260ee09f063e50aba4aa52da0f4403f646b76f11
Augmented Stupidity <134770379+AugmentedStupidity@users.noreply.github.com>
2023-08-10 15:48:43 -0400

    Here we go again;)

---
+++ README.md
@@ -0,0 +1,76 @@
+# Runpod SD-WebUI One Script Installer
+
+## Getting started
+
+**You need to use the PyTorch template (python3.10) in Runpod.**
+
+* Download the startscript file
+
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/AugmentedStupidity/runpod-sd-script/main/runpod_sd_webui_startscript.sh)"
+```
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/AugmentedStupidity/runpod-sd-script/main/text-setup.sh)"
+```
+
+
+```bash
+wget [https://gist.github.com/panta5/8706ce687576c4a80192fa3c05cffafc/raw/runpod_sd_webui_startscript.sh](https://raw.githubusercontent.com/AugmentedStupidity/runpod-sd-script/main/runpod_sd_webui_startscript.sh) -O /root/runpod.sh && chmod +x /root/runpod.sh && sed -i 's/NGROK_TOKEN_HERE/blablabla/g' /root/runpod.sh && bash /root/runpod.sh
+```
+
+```bash
+wget https://gist.github.com/panta5/8706ce687576c4a80192fa3c05cffafc/raw/runpod_sd_webui_startscript.sh -O /root/runpod_startscript.sh
+```
+
+* Modify the authtoken in the startscript.
+
+> If your authtoken is *2LVuzCWgjGPQ94uEKJ4uEKJKVJy\_EKJKVJygjGPQ94ubvJW9Y* then enter it like this
+>
+> DO NOT MODIFY *NGROK_TOKEN_HERE*
+
+```bash
+sed -i 's/NGROK_TOKEN_HERE/2LVuzCWgjGPQ94uEKJ4uEKJKVJy_EKJKVJygjGPQ94ubvJW9Y/g' /root/runpod_startscript.sh
+```
+
+* Grant the startscript execution privileges
+
+```bash
+chmod +x /root/runpod_startscript.sh
+```
+
+* Run the startscript
+
+```bash
+bash /root/runpod_startscript.sh
+```
+
+* Wait until it says it's done, as shown below.
+
+
+
+* Run the WebUI
+
+```bash
+bash /workspace/stable-diffusion-webui/start.sh
+```
+
+If you run it, you can see the Ngrok URL below.
+Now enjoy it.
+
+> Once it's installed, we'll only run it with the */workspace/stable-diffusion-webui/start.sh* file from now on.
+
+
+
+
+## Credit
+
+1. https://github.com/AUTOMATIC1111/stable-diffusion-webui
+2. https://stackoverflow.com/questions/238073/how-to-add-a-progress-bar-to-a-shell-script
+3. https://stackoverflow.com/questions/27162552/how-to-run-ngrok-in-background
+4. https://huggingface.co/Linaqruf/anything-v3.0/
+5. https://huggingface.co/hakurei/waifu-diffusion-v1-4/
+6. https://huggingface.co/datasets/gsdf/EasyNegative/
+7. https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+8. https://github.com/panta5
+
+thx.
---
+++ runpod_sd_webui_startscript.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+apt update
+apt install libgl1 -yq
+apt update && apt install -y nano libgl1 ffmpeg neofetch glances htop wget git python3 python3-venv pip ocl-icd-libopencl1 nvidia-cuda-toolkit hashcat hcxdumptool && ffmpeg -version
+apt install --no-install-recommends google-perftools -y
+
+# Ngrok
+#curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
+#https://ngrok.com/download
+curl --header 'Host: bin.equinox.io' --user-agent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://dashboard.ngrok.com/' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: cross-site' --header 'Sec-Fetch-User: ?1' 'https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz' --output 'ngrok-v3-stable-linux-amd64.tgz'
+
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+mv ./ngrok /usr/bin/ngrok
+# Ngrok 
+ngrok config add-authtoken NGROK_TOKEN_HERE
+echo "authtoken: NGROK_TOKEN_HERE" > /workspace/ngrok.yml
+# 
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /workspace/stable-diffusion-webui/
+# 
+wget https://huggingface.co/Linaqruf/anything-v3.0/resolve/main/anything-v3-fp16-pruned.safetensors -O /workspace/stable-diffusion-webui/models/Stable-diffusion/anything-v3-fp16-pruned.safetensors
+# VAE 
+wget https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/main/vae/kl-f8-anime2.ckpt -O /workspace/stable-diffusion-webui/models/VAE/kl-f8-anime2.ckpt
+# EasyNegative 
+wget https://huggingface.co/datasets/gsdf/EasyNegative/resolve/main/EasyNegative.safetensors -O /workspace/stable-diffusion-webui/embeddings/EasyNegative.safetensors
+# 
+git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git /workspace/stable-diffusion-webui/extensions/a1111-sd-webui-tagcomplete/
+# venv
+#python -m venv /workspace/stable-diffusion-webui/venv --without-pip
+# venv activate
+#source /workspace/stable-diffusion-webui/venv/bin/activate
+#curl https://bootstrap.pypa.io/get-pip.py | python
+# 
+#python -m pip install -r /workspace/stable-diffusion-webui/requirements_versions.txt
+# start.sh 
+#wget https://gist.github.com/panta5/b0cbddee37d6839561ad1b6a2d353a5b/raw/start.sh -O /workspace/stable-diffusion-webui/start.sh
+#wget https://gist.githubusercontent.com/panta5/6239382f45a773d75a9788efd1c940a9/raw/continue.sh -O /workspace/stable-diffusion-webui/continue.sh
+#chmod +x /workspace/stable-diffusion-webui/start.sh
+#chmod +x /workspace/stable-diffusion-webui/continue.sh
+# 
+###############################################################################################
+
+cd /workspace/stable-diffusion-webui/models/Stable-diffusion
+#git lfs install
+#git clone https://huggingface.co/prompthero/openjourney-v4
+#git pull https://huggingface.co/prompthero/openjourney-v4
+wget https://huggingface.co/prompthero/openjourney-v4/resolve/main/openjourney-v4.ckpt
+
+# https://huggingface.co/SG161222/Realistic_Vision_V1.4
+git lfs install
+git clone https://huggingface.co/SG161222/Realistic_Vision_V1.4
+git pull https://huggingface.co/SG161222/Realistic_Vision_V1.4
+cd /workspace/stable-diffusion-webui/models/Stable-diffusion/Realistic_Vision_V1.4
+rm Realistic_Vision_V1.4-*
+#rm Realistic_Vision_V1.4.safetensors
+
+#cd /workspace/stable-diffusion-webui/
+#bash webui.sh --xformers --api --share
+
+#cd /workspace/models/
+cd text-generation-webui/models/
+git lfs install
+git clone https://huggingface.co/TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ
+git pull https://huggingface.co/TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ
+git clone https://huggingface.co/TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ
+git pull https://huggingface.co/TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ
+git clone https://huggingface.co/TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ
+git pull https://huggingface.co/TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ
+git lfs install
+git clone https://huggingface.co/TheBloke/llama-2-70b-Guanaco-QLoRA-GPTQ
+git pull https://huggingface.co/TheBloke/llama-2-70b-Guanaco-QLoRA-GPTQ
+git clone https://huggingface.co/georgesung/llama2_7b_chat_uncensored
+git pull https://huggingface.co/georgesung/llama2_7b_chat_uncensored
+
+python3 -m pip install --upgrade pip
+
+cd /root/text-generation-webui/
+pip install -r requirements.txt
+cd extensions/
+cd api/
+pip install -r requirements.txt
+cd ..
+cd elevenlabs_tts/
+pip install -r requirements.txt
+cd ..
+cd google_translate/
+pip install -r requirements.txt
+cd ..
+cd silero_tts/
+pip install -r requirements.txt
+cd ..
+cd whisper_stt/
+pip install -r requirements.txt
+cd ..
+cd openai/
+pip install -r requirements.txt
+cd ..
+cd superbooga/
+pip install -r requirements.txt
+cd ..
+ls -la
+
+
+#=================================
+#sed -i "/#fi/i \n
+#alias restartob='bash /restart-text-generation-webui.sh' \n
+#alias editmodel='nano /workspace/run-text-generation-webui.sh' \n
+#alias restartsd='bash /workspace/stable-diffusion-webui/webui.sh --xformers --api --share' \n
+#alias aliasedit='nano ~/.bashrc' \n
+#alias aliasreload='source ~/.bashrc' \n
+#alias pkill='kill -9' \n
+#alias netdatainstall='wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud' \n
+#alias netdatarestart='service netdata restart'/g" /root/.bashrc
+
+#source ~/.bashrc
+
+#alias restart='bash /restart-text-generation-webui.sh'
+#alias editmodel='nano /workspace/run-text-generation-webui.sh'
+#alias restartsd='bash /workspace/stable-diffusion-webui/webui.sh --xformers --api --share'
+#alias aliasedit='nano ~/.bashrc'
+#alias aliasreload='source ~/.bashrc'
+#alias pkill='kill -9'
+#alias netdatainstall='wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud'
+#alias netdatarestart='service netdata restart'
+source ~/.bashrc
+
+#=================================
+
+###############################################################################################
+apt-get clean
+sed -i 's/can_run_as_root=0/can_run_as_root=1/g' /workspace/stable-diffusion-webui/webui.sh
+echo "bash -c /workspace/stable-diffusion-webui/start.sh"
+echo "bash -c /workspace/stable-diffusion-webui/continue.sh   \"\e[0m "
+
+cd /workspace/stable-diffusion-webui/
+bash webui.sh --xformers --api --share
+
+echo YAY!!!!!!!!!!!
+sleep 5
+echo Dustin Did it!!!
+sleep 6
+echo Boop Boop
+sleep 3
+echo Press any key to Fuck Off!
+
+bash ~/scripts/restart-text-generation-webui.sh
+echo "bash ~/scripts/restart-text-generation-webui.sh"
+exit 0
